8d8264f1a10c4eb7d9f678c0a934fc564dc4dc6b
Janis Komuls <janis.komuls@gmail.com>
2013-10-18 15:52:06 +0300

    Guava learning tests added: CharMatcher, Functions, Predicates, Joiner. Also used Fest assertions.

---
+++ learning/src/main/java/guava/Country.java
@@ -0,0 +1,25 @@
+package guava;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class Country {
+
+	private String name;
+
+	private String capital;
+
+	Country(String name, String capital) {
+		checkNotNull(name);
+		checkNotNull(capital);
+		this.name = name;
+		this.capital = capital;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCapital() {
+		return capital;
+	}
+}
