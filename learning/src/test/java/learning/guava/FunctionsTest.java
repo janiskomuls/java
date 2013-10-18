fa14dd20e88164827737ac697c355e721a6c266b
Janis Komuls <janis.komuls@gmail.com>
2013-10-18 15:54:37 +0300

    Guava tests moved to learning package

---
+++ learning/src/test/java/learning/guava/FunctionsTest.java
@@ -0,0 +1,112 @@
+package learning.guava;
+
+import static com.google.common.base.Functions.compose;
+import static com.google.common.base.Functions.forMap;
+import static com.google.common.base.Strings.nullToEmpty;
+import static com.google.common.collect.Collections2.transform;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+
+
+import java.util.Collection;
+import java.util.Map;
+
+import learning.guava.Country;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Guava: Functions, Collections2, ImmutableList, ImmutableMap <br>
+ * Hamcrest matchers
+ * 
+ */
+public class FunctionsTest {
+
+	Country latvia;
+	Country argentina;
+	Country jamaica;
+
+	ImmutableList<Country> countries;
+
+	@Before
+	public void setUp() {
+		latvia = new Country("Latvia", "Riga");
+		argentina = new Country("Argentina", "Buenos Aires");
+		jamaica = new Country("Jamaica", "Kingston");
+		countries = ImmutableList.of(latvia, argentina, jamaica);
+	}
+
+	@Test
+	public void shouldConvertCountriesToReversedAndCapitalizedNamesListUsingComposedFunction() {
+		Function<Country, String> composedFunction = createComposedFunctionToGetReversedAndCapitalizedNamesList();
+
+		Collection<String> reversedAndCapitalizedNames = transform(countries, composedFunction);
+
+		assertThat(reversedAndCapitalizedNames, contains("AIVTAL", "ANITNEGRA", "ACIAMAJ"));
+	}
+
+	@Test
+	public void shouldConvertCountriesMapToCapitalsListUsingForMapFunction() {
+		Collection<String> capitals = transform(getCountryNamesList(), capitalsFromCountriesFunction());
+
+		assertThat(capitals, contains("Riga", "Buenos Aires", "Kingston"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldThrowExceptionWhileConvertCountriesMapToCapitalsListUsingForMapFunction() {
+		Collection<String> capitals = transform(asList("Latvia", "Uganda"), capitalsFromCountriesFunction());
+
+		assertThat(capitals, contains("Riga"));
+	}
+
+	@Test
+	public void shouldSetUnknownToUnexistingKeysWhileConvertCountriesMapToCapitalsListUsingForMapFunction() {
+		Function<String, String> capitalsFromUnknownCountriesFunction = forMap(getCountriesMap(), "Unknown");
+
+		Collection<String> capitals = transform(asList("Latvia", "Uganda"), capitalsFromUnknownCountriesFunction);
+
+		assertThat(capitals, contains("Riga", "Unknown"));
+	}
+
+	private Function<String, String> capitalsFromCountriesFunction() {
+		return forMap(getCountriesMap());
+	}
+
+	private Function<Country, String> createComposedFunctionToGetReversedAndCapitalizedNamesList() {
+		Function<Country, String> uppercaseFunction = new Function<Country, String>() {
+			public String apply(Country country) {
+				String name = nullToEmpty(country.getName());
+				return name.toUpperCase();
+			}
+		};
+
+		Function<String, String> reverseFunction = new Function<String, String>() {
+			public String apply(String input) {
+				input = nullToEmpty(input);
+				return new StringBuilder(input).reverse().toString();
+			}
+		};
+
+		return compose(reverseFunction, uppercaseFunction);
+	}
+
+	private Collection<String> getCountryNamesList() {
+		Function<Country, String> countryNameFunction = new Function<Country, String>() {
+			public String apply(Country country) {
+				return nullToEmpty(country.getName());
+			}
+		};
+
+		return transform(countries, countryNameFunction);
+	}
+
+	private Map<String, String> getCountriesMap() {
+		return ImmutableMap.of(latvia.getName(), latvia.getCapital(), argentina.getName(), argentina.getCapital(), jamaica.getName(), jamaica.getCapital());
+	}
+}
---
+++ learning/src/test/java/learning/guava/PredicatesTest.java
@@ -0,0 +1,108 @@
+package learning.guava;
+
+import static com.google.common.base.Predicates.and;
+import static com.google.common.base.Predicates.containsPattern;
+import static com.google.common.base.Predicates.or;
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.google.common.collect.Iterables.all;
+import static com.google.common.collect.Iterables.filter;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+
+import java.util.Iterator;
+
+import learning.guava.Country;
+
+import org.fest.assertions.core.Condition;
+import org.junit.Before;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Guava: Predicates, String, Iterables <br>
+ * Fest assertions
+ * 
+ */
+public class PredicatesTest {
+
+	Country latvia;
+	Country argentina;
+	Country jamaica;
+
+	ImmutableList<Country> countries;
+
+	Predicate<Country> latviaPredicate;
+	Predicate<Country> kingstonPredicate;
+
+	@Before
+	public void setUp() {
+		latvia = new Country("Latvia", "Riga");
+		argentina = new Country("Argentina", "");
+		jamaica = new Country("Jamaica", "Kingston");
+		countries = ImmutableList.of(latvia, argentina, jamaica);
+
+		latviaPredicate = new Predicate<Country>() {
+			public boolean apply(Country country) {
+				return latvia.getName().equals(country.getName());
+			}
+		};
+
+		kingstonPredicate = new Predicate<Country>() {
+			public boolean apply(Country country) {
+				return jamaica.getCapital().equals(country.getName());
+			}
+		};
+	}
+
+	public void shouldUseCustomPredicate() {
+		// given
+		Predicate<Country> capitalCityProvidedPredicate = new Predicate<Country>() {
+			public boolean apply(Country country) {
+				return !isNullOrEmpty(country.getCapital());
+			}
+		};
+
+		// when
+		boolean allCountriesSpecifyCapitalCity = all(countries, capitalCityProvidedPredicate);
+
+		// then
+		assertThat(allCountriesSpecifyCapitalCity).isFalse();
+	}
+
+	public void shouldNotFilterCountriesUsingComposedByAndPredicate() {
+		// given
+		Condition<Iterable<Country>> anyCountry = new Condition<Iterable<Country>>() {
+			public boolean matches(Iterable<Country> country) {
+				return country.iterator().hasNext();
+			}
+		};
+
+		Predicate<Country> composedPredicate = and(latviaPredicate, kingstonPredicate);
+
+		// when
+		Iterable<Country> countriesWithIncorrectCapital = filter(countries, composedPredicate);
+
+		// then
+		assertThat(countriesWithIncorrectCapital).doesNotHave(anyCountry);
+	}
+
+	public void shouldFilterCountriesUsingComposedByOrPredicate() {
+		// given
+		Predicate<Country> composedPredicate = or(latviaPredicate, kingstonPredicate);
+
+		// when
+		Iterable<Country> fileteredCountries = filter(countries, composedPredicate);
+
+		// then
+		Iterator<Country> iterator = fileteredCountries.iterator();
+		assertThat(iterator.next()).isEqualTo(latvia);
+		assertThat(iterator.next()).isEqualTo(jamaica);
+	}
+
+	public void shouldCheckPattern() {
+		Predicate<CharSequence> twoDigitsPredicate = containsPattern("\\d\\d*!$");
+
+		assertThat(twoDigitsPredicate.apply("I am 18 years old!")).isTrue();
+	}
+}
